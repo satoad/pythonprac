0228bf465d7008d0d0d5eff2b068d69b2b21e6b9
satoad <steamtoad08@gmail.com>
2022-10-11 01:13:49 +0300

    1 and 2 task

--- 20221004/1/prog.py
+++ 20221004/1/prog.py
@@ -1,12 +1,14 @@
 def bin_search(str1, str2):
     n = len(str2)
     if n > 0:
-        if str1 == str2[int(n/2)]:
+        if str1 == str2[n // 2]:
             return True
-        elif str1 > str2[int(n/2)]:
-            return bin_search(str1, str2[int(n/2):])   
+        elif n == 1:
+            return False
+        elif str1 > str2[n // 2]:
+            return bin_search(str1, str2[n // 2:])   
         else:
-            return bin_search(str1, str2[:int(n/2)])
+            return bin_search(str1, str2[:n // 2])
     else:
         return False
 
---
+++ 20221004/2/prog.py
@@ -0,0 +1,13 @@
+def pareto(*pairs):
+    res = []
+
+    for i in pairs:
+        for j in pairs:
+            if i[0] <= j[0] and i[1] <= j[1] and (i[0] < j[0] or i[1] < j[1]):
+                break           
+            else:
+                res.append(i)
+
+    return tuple(res)
+
+print(pareto(*eval(input())))
