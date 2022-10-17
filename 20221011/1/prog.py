c9206dbeecc33e7f3afa7e2603d5eff29ff97c1a
satoad <steamtoad08@gmail.com>
2022-10-17 23:33:52 +0300

    1 task

--- 20221011/1/prog.py
+++ 20221011/1/prog.py
@@ -1,3 +1,13 @@
-inp = eval(input())
+from fractions import Fraction
 
 
+def is_root(x, b, coeffs, Type):
+    numerator = sum([Type(coeff) * Type(x) ** i for i, coeff in enumerate(reversed(coeffs[1:int(coeffs[0]) + 2]))])
+
+    denominator = sum([Type(coeff) * Type(x) ** i for i, coeff in enumerate(reversed(coeffs[int(coeffs[0]) + 3:]))])
+
+    return numerator / denominator == Type(b) if denominator else False
+
+
+s, w, *pol_coeffs = input().split(',')
+print(is_root(s, w, pol_coeffs, Fraction))
