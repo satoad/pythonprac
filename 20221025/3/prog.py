ddb45df82bb7bfe0ea7809dc2eb0325d5caae6c4
satoad <steamtoad08@gmail.com>
2022-10-30 00:49:28 +0300

    one more fix

--- 20221025/3/prog.py
+++ 20221025/3/prog.py
@@ -2,12 +2,13 @@ from itertools import combinations_with_replacement
 
 sample = 'TOR'
 num = eval(input())
+
 if num > len(sample) * 2:
-    text = sample * (num//len(sample))
+    text = sample * (num // len(sample))
     if num % len(sample) != 0:
         text += sample
 
-    ans = list(sorted(set((filter(lambda x: ''.join(x).count(sample) == 2, combinations_with_replacement(text, num)))))) #cringe
+    ans = list(sorted(set((filter(lambda x: ''.join(x).count(sample) >= 2, combinations_with_replacement(text, num)))))) #cringe
 
     for i in range(len(ans)):
         print(*ans[i], sep='', end='')
