d2c506162b386376a4c541ce014b7158737beec5
satoad <steamtoad08@gmail.com>
2022-11-14 23:33:51 +0300

    cringe v1.03

--- 20221108/1/prog.py
+++ 20221108/1/prog.py
@@ -7,14 +7,15 @@ class DivStr(UserString):
     def __floordiv__(self, other):
         trim = len(self) // other
         res = []
-        for i in range(trim):
-            res.append(self[i * other:(i + 1) * other])
+        for i in range(other):
+            res.append(self[i * trim:(i + 1) * trim])
 
         return res
 
     def __mod__(self, other):
         trim = len(self) % other
         return self[-trim:]
+
 
 import sys
 exec(sys.stdin.read())
