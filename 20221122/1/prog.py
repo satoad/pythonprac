b40417ff7cb5529544fc4d612bea5832ee5a42e5
satoad <steamtoad08@gmail.com>
2022-11-28 14:32:09 +0300

    cringe v0.2

--- 20221122/1/prog.py
+++ 20221122/1/prog.py
@@ -1,1 +1,7 @@
-eval(input()) # я сделяль
+import sys
+
+a = sys.stdin.buffer.read()
+sys.stdout.buffer.write(a[0:1])
+
+for i in sorted([a[1:][i * (len(a) - 1) // a[0]:(i+1) * (len(a) - 1) // a[0]] for i in range(0, a[0])]):
+    sys.stdout.buffer.write(i)
