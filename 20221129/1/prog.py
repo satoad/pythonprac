4bafd28d028e44cfb57a5d79c3dbd6f4e1f068bc
satoad <steamtoad08@gmail.com>
2022-12-05 06:59:49 +0300

    prog 1

--- 20221129/1/prog.py
+++ 20221129/1/prog.py
@@ -1,1 +1,19 @@
-class dump(type): # а дальше берем и обмазываемся
+import types
+
+
+def dec(fun):
+    def new_f(*args, **kwargs):
+        print(f'{fun.__name__}: {args[1:]}, {kwargs}')
+        return fun(*args, **kwargs)
+    return new_f
+
+
+class dump(type):
+    def __new__(cls, clsname, bases, attrs, **kwds):
+        new_attrs = {}
+        for attr, v in attrs.items():
+            if not isinstance(v, types.FunctionType):
+                new_attrs[attr] = v
+            else:
+                new_attrs[attr] = dec(v)
+        return type(clsname, bases, new_attrs)
